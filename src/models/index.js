d4cdf6b6b8659b1c2013c5258836d37fea87c5be
leonidaspedro <leonidas@unochapeco.edu.br>
2023-10-26 14:59:55 -0300

    index correcao nomes

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,13 @@
 import Atividade from './Atividade';
-import Localizacao from './Localisacao';
+import Localizacao from './Localizacao';
 import TipoUsuario from './TipoUsuario';
 import Usuario from './Usuario';
 
 (async () => {
 
-    // Atividade.sync({ force: true });
-    // Localizacao.sync({ force: true });
-    // Usuario.sync({ force: true });
-    // TipoUsuario.sync({ force: true });
+    Atividade.sync({ force: true });
+    Localizacao.sync({ force: true });
+    Usuario.sync({ force: true });
+    TipoUsuario.sync({ force: true });
     
 })();    
